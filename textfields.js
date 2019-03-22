fb07709b7314f31a2359e26e7d6b137c8f9e683c
whiteout81 <30988414+whiteout81@users.noreply.github.com>
2019-03-22 09:51:35 -0400

    Removed the need for the update button

--- textfields.js
+++ textfields.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
 
 	var lastForm;
+	// Only for the first one in the drop down menu
+	lastForm = $("#conferenceForm");
 
-    $("#update").click(function() {
+    $("#tableDropdown").change(function () {
     	$(lastForm).css("display", "none");
     	var currentForm = '#' + $('#tableDropdown').val() + "Form";
     	$(currentForm).css("display", "block");
     	lastForm = currentForm;
     });
+
+     
+    lastForm.css("display", "block");
 });
